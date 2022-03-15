0696d577ba18ea481a1c2017cfd1904c88a4fced
prathmesh264 <99034151+prathmesh264@users.noreply.github.com>
2022-03-15 19:57:10 +0530

    initial commit

---
+++ mega.js
@@ -0,0 +1,38 @@
+let button = document.getElementById('login');
+        button.addEventListener('click', inputMsg)
+        function inputMsg(){
+            let user1 = prompt('create your user name here');
+            let password1 = prompt('create your password here');
+            let password2 = prompt('Rewrite to Confirm password');
+            let Account = alert("SignUp succesfully !!! ")
+
+            function makeAccount(user, password){
+                let Account = alert("your user name is " + use + " and password is " + passwor + " thank you for login in !");
+                return Account;
+            }
+            let login = makeAccount(user1, password1);
+        }
+
+        let button2 = document.getElementById('faq');
+        button2.addEventListener('click', inputMsg)
+        function inputMsg(){
+            let faq = alert('CLICK HERE FOR FAQ');
+            
+        }
+
+        let button3 = document.getElementById('payment');
+        button3.addEventListener('click', inputMsg)
+        function inputMsg(){
+            let payment = alert('do your PAYMENT HERE')
+        }
+
+        let button4 = document.getElementById('contact');
+        button4.addEventListener('click', inputMsg)
+        function inputMsg(){
+            let payment = alert('give your contact details')
+        }
+        
+
+
+
+    
